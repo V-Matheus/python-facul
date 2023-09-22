c065b88738335cac12d8c955208309fb13301854
Victor Matheus Lopes de Sousa <20232014050001@ifrn.local>
2023-09-22 19:48:56 -0300

    ex07

---
+++ lista-exercícios01/ex_07.py
@@ -0,0 +1,29 @@
+x = int(input('Digite um número de 1 a 12: '))
+
+if 0 < x <= 12:
+    if x==1:
+        print('Janeiro')
+    if x==2:
+        print('Fevereiro')
+    if x==3:
+        print('Março')
+    if x==4:
+        print('Abril')
+    if x==5:
+        print('Maio')
+    if x==6:
+        print('Junho')
+    if x==7:
+        print('Julho')
+    if x==8:
+        print('Agosto')
+    if x==9:
+        print('Setembro')
+    if x==10:
+        print('Outubro')
+    if x==11:
+        print('Novembro')
+    if x==12:
+        print('Dezembro')
+else: 
+    print('Digite um valor válido')
