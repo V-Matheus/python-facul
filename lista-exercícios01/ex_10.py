73279033660c1fdd7204639ec2835994b40ccd70
Victor Matheus Lopes de Sousa <20232014050001@ifrn.local>
2023-09-26 19:20:05 -0300

    ex10 update

--- lista-exercícios01/ex_10.py
+++ lista-exercícios01/ex_10.py
@@ -3,6 +3,7 @@ from re import I
 
 cateto1 = float(input('Digite o valor do primeiro cateto: '))
 cateto2 = float(input('Digite o valor do segundo cateto: '))
-
 h = (cateto1**2 + cateto2**2)**0.5
-print(f'A hipotenuza do {cateto1} e {cateto2} é igual a {h:.2f}')
+
+if cateto1 > 0 and cateto2 > 0:
+    print(f'A hipotenuza do {cateto1} e {cateto2} é igual a {h:.2f}')
