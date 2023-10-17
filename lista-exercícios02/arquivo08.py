45245e9bd7755293c2443c155e188d2d2023b469
Victor Matheus Lopes de Sousa <20232014050001@ifrn.local>
2023-10-17 16:57:00 -0300

    ex08

---
+++ lista-exercícios02/arquivo08.py
@@ -0,0 +1,12 @@
+n = int(input('Digite a quantidade de números: '))
+i = int(input('Digite o primeiro valor: '))
+j = int(input('Digite o segundo valor: '))
+x = 0
+
+if n != 0 or i != 0 or j != 0:
+    while x < n:
+        print(f'{x * j * i}', end=', ')
+        x += 1
+    
+else: 
+    print('digite um valor positivo')
