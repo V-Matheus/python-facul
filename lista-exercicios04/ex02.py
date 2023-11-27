59d5c43c48f5d2a9694fd598c491cbc74ce451c0
V-Matheus <victormatheus507@gmail.com>
2023-11-27 17:27:39 -0300

    ex02

--- lista-exercicios04/ex02.py
+++ lista-exercicios04/ex02.py
@@ -4,11 +4,11 @@ n = int(input('Digite o tamanho da lista: '))
 listaRandom = [random.randint(0,99) for _ in range(n)]
 print(f'Lista original: {listaRandom}')
 
-# for i in range(1, len(listaRandom)):
-#     key = listaRandom[i]
-#     j = i - 1
-#     while j >= 0 and key < listaRandom[j]:
-#         listaRandom[j + 1] = listaRandom[j]
-#         j -= 1
-#     listaRandom[j + 1] = key
-# print("Lista Ordenada:", listaRandom)
+for i in range(len(listaRandom)):
+    for j in range(0, len(listaRandom)-i-1):
+      # Verica se o número atual é maior que o próximo número
+        if listaRandom[j] > listaRandom[j+1]:
+            # Troca os elementos se estiverem fora de ordem
+            listaRandom[j], listaRandom[j+1] = listaRandom[j+1], listaRandom[j]
+
+print("Lista Ordenada:", listaRandom)
