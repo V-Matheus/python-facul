6147388668fc48c319caad6e6877e9dd049c3b18
Victor Matheus <victormatheus507@gmail.com>
2023-11-30 11:31:04 -0300

    ex04 media correta

--- lista-exercicios04/ex03/ex03.py
+++ lista-exercicios04/ex03/ex03.py
@@ -1,12 +1,14 @@
 from dolar import cotacoes_dolar
 
-# Percorre toda a lista de dolar e recolhe os dados
+# Criar um dicionario que tem a relação entre mês e valor
 valores_por_mes = {str(i).zfill(2): [] for i in range(1, 13)}
 for dado in cotacoes_dolar:
 
   # Separa o mes
   data = dado[0]
   mes = data.split('-')[1]
+  
+  valores_por_mes[mes].append(dado[1])
 
 for mes, valores in valores_por_mes.items():
   media = round(sum(valores) / len(valores_por_mes[mes]), 4)
