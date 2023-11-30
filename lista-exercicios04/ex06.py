2d43e58be736c1a289c8c8f0a49de449da61746e
Victor Matheus <victormatheus507@gmail.com>
2023-11-30 10:29:16 -0300

    colocando comentários ao ex06

--- lista-exercicios04/ex06.py
+++ lista-exercicios04/ex06.py
@@ -8,9 +8,12 @@ else:
     moedas = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1]
     moedas_stg = ['R$ 100,00', 'R$ 50,00', 'R$ 20,00', 'R$ 10,00', 'R$ 5,00', 'R$ 2,00', 'R$ 1,00', 'R$ 0,50', 'R$ 0,25', 'R$ 0,10', 'R$ 0,05', 'R$ 0,01']
 
+# Irá comparar as duas listas
     for moeda, moeda_stg in zip(moedas, moedas_stg):
+      # Faz uma divisão inteira para saber a quantidade a partir do valor digitado em centavos e depois retorna o resto
         quantidade = centavos // moeda
         centavos = centavos % moeda
 
+      # Se existir notas ou moedas para aquele valor ele irá mostrar
         if quantidade > 0:
             print(f'Com esse valor você conseguirá sacar {quantidade} {moeda_stg}')
---
+++ lista-exercicios04/ex10.py
@@ -0,0 +1,20 @@
+import random
+
+# Solicitar ao usuário a quantidade de listas e elementos
+qtd_listas = int(input("Digite a quantidade de listas na matriz: "))
+qtd_elementos = int(input("Digite a quantidade de elementos em cada lista: "))
+
+# Criar matriz aleatória
+matriz_original = [[random.randint(1, 10) for _ in range(qtd_elementos)] for _ in range(qtd_listas)]
+
+# Exibir matriz original
+print("Matriz Original:")
+for linha in matriz_original:
+    print(linha)
+
+# Calcular e exibir matriz transposta
+matriz_transposta = [[matriz_original[j][i] for j in range(len(matriz_original))] for i in range(len(matriz_original[0]))]
+
+print("Matriz Transposta:")
+for linha in matriz_transposta:
+    print(linha)
