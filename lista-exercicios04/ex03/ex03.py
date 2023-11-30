7e98d4115ca3513d280c6c685ad8a03f8226b68c
Victor Matheus <victormatheus507@gmail.com>
2023-11-30 11:25:19 -0300

    ex04 media

--- lista-exercicios04/ex03/ex03.py
+++ lista-exercicios04/ex03/ex03.py
@@ -1,54 +1,14 @@
 from dolar import cotacoes_dolar
 
 # Percorre toda a lista de dolar e recolhe os dados
-valores = []
+valores_por_mes = {str(i).zfill(2): [] for i in range(1, 13)}
 for dado in cotacoes_dolar:
 
   # Separa o mes
   data = dado[0]
   mes = data.split('-')[1]
+
+for mes, valores in valores_por_mes.items():
+  media = round(sum(valores) / len(valores_por_mes[mes]), 4)
+  print(f'No {mes}º mês: Média: {media}')
   
-
-  if mes == '01':
-    valores.append(dado[1])
-    janeiroList = sum(valores)
-    janeiro = f'Janeiro: {janeiroList}'
-    # print(janeiro)
-    valores.clear()
-
-  if mes == '02':
-    valores.append(dado[1])
-    FevereiroList = sum(valores)
-    fevereiro = f'Fevereiro: {valores}'
-
-  if mes == '03':
-    valores.append(dado[1])
-
-  if mes == '04':
-    valores.append(dado[1])
-
-  if mes == '05':
-    valores.append(dado[1])
-
-  if mes == '06':
-    valores.append(dado[1])
-
-  if mes == '07':
-    valores.append(dado[1])
-
-  if mes == '08':
-    valores.append(dado[1])
-
-  if mes == '09':
-    valores.append(dado[1])
-
-  if mes == '10':
-    valores.append(dado[1])
-  
-  if mes == '11':
-    valores.append(dado[1])
-
-  if mes == '12':
-    valores.append(dado[1])
-print(janeiro)
-print(fevereiro)
