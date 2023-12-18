1f2aa7ddf078932b5fe4a4d6b10f001dbd51971e
V-Matheus <victormatheus507@gmail.com>
2023-12-18 16:32:18 -0300

    criando dicionarios para cada posicao

--- lista-exercicios05/ex01/ex01.py
+++ lista-exercicios05/ex01/ex01.py
@@ -1,33 +1,54 @@
-from ctypes import pointer
+from turtle import pos
 import requests, os
 from datetime import date
+
 strURL = 'https://api.cartolafc.globo.com/atletas/mercado'
 dictCartola = requests.get(strURL, verify=False).json()
 
-
 atletas = dictCartola.get('atletas')
-atleta = {'nome': '', 'num': 0, 'media': 0, 'pontuacao': 0}
-
+maiores_pontuacoes = {}
+posicoes = {1: 'goleiro', 2: 'lateral', 3: 'zagueiro', 4: 'meia', 5: 'atacante', 6: 'Técnico'}
 
 for atleta in atletas:
-  jogos_num = atletas[atleta]['jogos_num']
-  media_num = atletas[atleta]['media_num']
-  maior_pontuacao = jogos_num * media_num
+    nome = atleta['nome']
+    posicao_id = atleta['posicao_id']
+    jogos_num = atleta['jogos_num']
+    media_num = atleta['media_num']
+    maior_pontuacao = round(jogos_num * media_num, 2)
 
-  # atletas[atleta]['jogos_num'] += dicionario
-  print(maior_pontuacao)
+   # Cria um novo dicionário para cada jogador
+    info_jogador = {'nome': nome, 'posicao': posicoes[posicao_id]}
 
-# print(jogos_num, media_num)
-# print(f'\n')
+    # Armazena a maior pontuação para cada jogador no dicionário maiores_pontuacoes
+    maiores_pontuacoes[nome] = {'pontuacao': maior_pontuacao, 'info_jogador': info_jogador}
 
+# Ordena os jogadores por maior pontuação
+melhores_atletas = sorted(maiores_pontuacoes.items(), key=lambda x: x[1]['pontuacao'], reverse=True)
 
+# Criar dicionários separados para cada posição
+melhores_goleiros = {}
+melhores_laterais = {}
+melhores_zagueiros = {}
+melhores_meias = {}
+melhores_atacantes = {}
+melhores_tecnicos = {}
 
+# Iterar sobre os melhores jogadores
+for jogador, info in melhores_atletas:
+    posicao = info['info_jogador']['posicao']
 
-# while True:
-#   ano = int(input('Digite o ano que deseja: '))
-#   if(ano == int(date.today().year)):
-#     break
-
+    # Adicionar o jogador ao dicionário da posição correspondente
+    if posicao == 'goleiro':
+        melhores_goleiros[jogador] = info
+    elif posicao == 'lateral':
+        melhores_laterais[jogador] = info
+    elif posicao == 'zagueiro':
+        melhores_zagueiros[jogador] = info
+    elif posicao == 'meia':
+        melhores_meias[jogador] = info
+    elif posicao == 'atacante':
+        melhores_atacantes[jogador] = info
+    elif posicao == 'Técnico':
+        melhores_tecnicos[jogador] = info
 
 esquema_possiveis = [343, 352, 433, 442, 451, 532, 541]
-# print(ano)
