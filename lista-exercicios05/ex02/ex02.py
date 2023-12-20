e56f7bb93fa6a29506c06097bff0548b5f3eabb3
V-Matheus <victormatheus507@gmail.com>
2023-12-20 16:18:23 -0300

    criando os arquivos

--- lista-exercicios05/ex02/ex02.py
+++ lista-exercicios05/ex02/ex02.py
@@ -14,9 +14,14 @@ listInfo = []
 listDoc = []
 
 for servidor in dictServidores:
+    # Organiza dados e separa por ;
     servidor_info = servidor[0][1].split(';')
+
+    # Ignora os dados 'None'
     sigla_campus = servidor_info[11] if len(servidor_info) >= 12 and servidor_info[11] and not servidor_info[11].isdigit() else None
     categoria = servidor[0][1].split(';')[0]
+
+    # Se encontrar o campus adiciona + 1 para o tipo de servidor
     if sigla_campus and not sigla_campus.isdigit():
         campus_encontrado = False
         for campus_entry in listInfo:
@@ -27,7 +32,8 @@ for servidor in dictServidores:
                     campus_entry[1][categoria] = 1
                 campus_encontrado = True
                 break
-
+            
+        # se não achar o campus adiciona a lista
         if not campus_encontrado:
             listInfo.append([sigla_campus, {'docente': 0, 'tecnico_administrativo': 0, 'estagiario': 0, 'indefinida': 0}])
             for campus_entry in listInfo:
@@ -40,7 +46,7 @@ for servidor in dictServidores:
 for servidor_info in dictServidores:
     # Certifica-se de que há pelo menos 2 elementos e que o segundo não é vazio
     if len(servidor_info[0][1].split(';')) >= 2 and servidor_info[0][1].split(';')[1]:
-        disciplina_ingresso = servidor_info[0][1].split(';')[1]  # Agora considerando a disciplina na posição 1
+        disciplina_ingresso = servidor_info[0][1].split(';')[1]
         encontrado = False
         for item in listDoc:
             if item[0] == disciplina_ingresso:
@@ -77,4 +83,30 @@ with open(caminho_arquivo_csv, mode='w', newline='', encoding='UTF-8') as arquiv
     for docente_entry in listDoc:
         escritor_csv.writerow([docente_entry[0], docente_entry[1]])
 
-print(f"Os dados dos docentes foram salvos na pasta raiz.")
+# Caminho do arquivo CSV para servidores
+caminho_arquivo_csv_servidores = os.path.join(strDiretorio, "servidores_campi.csv")
+
+# Escreve o arquivo CSV para servidores
+with open(caminho_arquivo_csv_servidores, 'w', newline='', encoding='UTF-8') as arquivo_csv:
+    # Escreve cabeçalho
+    arquivo_csv.write(" - ".join(["Sigla do Campus", "Docente", 'tecnico_administrativo', 'estagiario', 'indefinida']) + '\n')
+
+    # Escreve dados
+    for campus_entry in listInfo:
+        arquivo_csv.write(" - ".join([campus_entry[0], str(campus_entry[1]['docente']), str(campus_entry[1]['tecnico_administrativo']), str(campus_entry[1]['estagiario']), str(campus_entry[1]['indefinida'])]) + '\n')
+
+print(f"Os dados dos servidores foram salvos em {caminho_arquivo_csv_servidores}.")
+
+# Caminho do arquivo CSV para docentes
+caminho_arquivo_csv_docentes = os.path.join(strDiretorio, "docentes_disciplinas.csv")
+
+# Escreve o arquivo CSV para docentes
+with open(caminho_arquivo_csv_docentes, 'w', newline='', encoding='UTF-8') as arquivo_csv:
+    # Escreve cabeçalho
+    arquivo_csv.write(" - ".join(['Disciplina', 'Quantidade de docentes da disciplina']) + '\n')
+
+    # Escreve dados
+    for docente_entry in listDoc:
+        arquivo_csv.write(" - ".join([docente_entry[0], str(docente_entry[1])]) + '\n')
+
+print(f"Os dados dos docentes foram salvos em {caminho_arquivo_csv_docentes}.")
