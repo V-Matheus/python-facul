f1a3eb9f5bf3ed6687d236321c8f23f1603d149a
V-Matheus <victormatheus507@gmail.com>
2023-12-21 16:55:25 -0300

    criando arquivo txt

--- lista-exercicios05/ex04/ex04.py
+++ lista-exercicios05/ex04/ex04.py
@@ -20,6 +20,7 @@ pasta_csv = os.path.join(caminho_script, 'serie_historica_anp')
 # Lista para armazenar as informações
 informacoes = []
 
+print('Carregando dados....')
 # Iterar sobre os arquivos na pasta
 for arquivo in os.listdir(pasta_csv):
         caminho_arquivo = os.path.join(pasta_csv, arquivo)
@@ -62,4 +63,51 @@ with open(caminho_arquivo_csv, 'w', encoding='utf-8') as csvfile:
     for info in informacoes:
         csvfile.write(';'.join(map(str, info.values())) + '\n')
 
-print(f"Dados salvos em: {caminho_arquivo_csv}")
+# Calculando média de valor de venda por bandeira e produto
+media_bandeira = {}
+# Calculando média de valor de venda por produto, região e ano
+media_produto_regiao = {}
+
+# Processando as informações
+for info in informacoes:
+    bandeira = info['Bandeira']
+    produto = info['Produto']
+    regiao = info['Regiao – Sigla']
+    ano = info['Data da Coleta'][-4:]  # Obtendo o ano dos dados
+    valor_venda = float(info['Valor de Venda'].replace(',', '.'))
+
+    # Calculando média por bandeira e produto
+    chave_bandeira_produto = f"{bandeira} - {produto} - {ano}"
+    if chave_bandeira_produto not in media_bandeira:
+        media_bandeira[chave_bandeira_produto] = {'soma': 0, 'quantidade': 0}
+    media_bandeira[chave_bandeira_produto]['soma'] += valor_venda
+    media_bandeira[chave_bandeira_produto]['quantidade'] += 1
+
+    # Calculando média por produto, região e ano
+    chave_produto_regiao = f"{produto} - {regiao} - {ano}"
+    if chave_produto_regiao not in media_produto_regiao:
+        media_produto_regiao[chave_produto_regiao] = {'soma': 0, 'quantidade': 0}
+    media_produto_regiao[chave_produto_regiao]['soma'] += valor_venda
+    media_produto_regiao[chave_produto_regiao]['quantidade'] += 1
+
+# Salvando média por bandeira e produto
+caminho_arquivo_media_bandeira = os.path.join(caminho_dados_estatisticos, 'media_bandeira.txt')
+with open(caminho_arquivo_media_bandeira, 'w', encoding='utf-8') as arquivo:
+    arquivo.write("bandeira;produto;ano;valor_medio_venda;quantidade_postos\n")
+    for chave, dados in media_bandeira.items():
+        bandeira, produto, ano = chave.split(' - ')
+        valor_medio_venda = dados['soma'] / dados['quantidade']
+        quantidade_postos = dados['quantidade']
+        arquivo.write(f"{bandeira};{produto};{ano};{valor_medio_venda:.2f};{quantidade_postos}\n")
+
+# Salvando média por produto, região e ano
+caminho_arquivo_media_produto_regiao = os.path.join(caminho_dados_estatisticos, 'media_produto_regiao.txt')
+with open(caminho_arquivo_media_produto_regiao, 'w', encoding='utf-8') as arquivo:
+    arquivo.write("produto;regiao;ano;valor_medio;quantidade_postos\n")
+    for chave, dados in media_produto_regiao.items():
+        produto, regiao, ano = chave.split(' - ')
+        valor_medio = dados['soma'] / dados['quantidade']
+        quantidade_postos = dados['quantidade']
+        arquivo.write(f"{produto};{regiao};{ano};{valor_medio:.2f};{quantidade_postos}\n")
+
+print(f'Seus dados foram salvos no caminho: {caminho_dados_estatisticos}')
